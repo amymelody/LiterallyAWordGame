fc3a4b32a47e9c6204504e51eb767cb0038fed2e
Josh DiGiovanni <jmdigiov@usc.edu>
2014-09-27 20:42:25 -0700

    Climbing works

--- Literally A Word Game/Assets/Scripts/Lee.cs
+++ Literally A Word Game/Assets/Scripts/Lee.cs
@@ -7,6 +7,7 @@ public class Lee : MonoBehaviour {
 
 	public float movementSpeed;
 	private bool canJump = false;
+	private bool onLadder = false;
 	private MovementDirection direction = MovementDirection.None;
 
 	// Use this for initialization
@@ -50,10 +51,22 @@ public class Lee : MonoBehaviour {
 			}
 		}
 
-		//Handle jumping
-		if (canJump && Input.GetKeyDown(KeyCode.W)) {
-			canJump = false;
-			setYVelocity(10f);
+		//Handle jumping & climbing
+		if (onLadder) {
+			if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.S)) {
+				setYVelocity(0);
+			}
+			if (Input.GetKeyDown (KeyCode.W)) {
+				setYVelocity (5);
+			}
+			if (Input.GetKeyDown (KeyCode.S)) {
+				setYVelocity (-5);
+			}
+		} else {
+			if (canJump && Input.GetKeyDown(KeyCode.W)) {
+				canJump = false;
+				setYVelocity(10f);
+			}
 		}
 	}
 
@@ -66,6 +79,22 @@ public class Lee : MonoBehaviour {
 				canJump = true;
 			}
 
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+
+		if (collider.gameObject.tag.Equals("Climbable")) {
+			onLadder = true;
+			rigidbody.useGravity = false;
+			setYVelocity(0);
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.tag.Equals("Climbable")) {
+			onLadder = false;
+			rigidbody.useGravity = true;
 		}
 	}
 
