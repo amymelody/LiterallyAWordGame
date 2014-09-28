30b7b02c811537af6a3a628f2ecaec836559a8bd
Josh DiGiovanni <jmdigiov@usc.edu>
2014-09-27 17:32:37 -0700

    Floor and Lee block

---
+++ Literally A Word Game/Assets/Scripts/Lee.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.D)) {
+			transform.rigidbody.velocity = new Vector3(10,0,0);
+		}
+	}
+}
